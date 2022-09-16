a34686f1d2349e5ea9040d21994fb057ff194ff0
cetteup <me@cetteup.com>
2022-09-16 18:55:06 +0200

    chore: Add initial project structure

---
+++ aspxstats/__init__.py
@@ -0,0 +1,9 @@
+"""
+aspxstats.
+Python library for retrieving stats of Battlefield 2 and Battlefield 2142 players.
+"""
+
+__version__ = '0.0.1'
+__author__ = 'cetteup'
+__credits__ = 'wilson212'
+__all__ = []
