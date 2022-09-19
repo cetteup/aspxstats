40e72a9c955ef459f9c0b27cb8fdb588564f453d
cetteup <me@cetteup.com>
2022-09-19 19:04:06 +0200

    feat: Add easy to use variants of methods not bound to client

--- aspxstats/bf2/__init__.py
+++ aspxstats/bf2/__init__.py
@@ -1,6 +1,20 @@
 from .client import AspxClient
+from .fetch import searchforplayers, searchforplayers_raw, getleaderboard, getleaderboard_raw
 from .types import StatsProvider, SearchMatchType, SearchSortOrder, LeaderboardType, ScoreLeaderboardId, \
     WeaponLeaderboardId, VehicleLeaderboardId, KitLeaderboardId
 
-__all__ = ['AspxClient', 'StatsProvider', 'SearchMatchType', 'SearchSortOrder', 'LeaderboardType',
-           'ScoreLeaderboardId', 'WeaponLeaderboardId', 'VehicleLeaderboardId', 'KitLeaderboardId']
+__all__ = [
+    'AspxClient',
+    'searchforplayers',
+    'searchforplayers_raw',
+    'getleaderboard',
+    'getleaderboard_raw',
+    'StatsProvider',
+    'SearchMatchType',
+    'SearchSortOrder',
+    'LeaderboardType',
+    'ScoreLeaderboardId',
+    'WeaponLeaderboardId',
+    'VehicleLeaderboardId',
+    'KitLeaderboardId'
+]
