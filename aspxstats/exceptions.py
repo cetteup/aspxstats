751f6127e47a211fbff81edd943a2e8c75c9b4a1
cetteup <me@cetteup.com>
2024-08-28 20:32:03 +0200

    feat: Include problematic path and value in validation error

--- aspxstats/exceptions.py
+++ aspxstats/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Optional, Any
+
+
 class Error(Exception):
     pass
 
@@ -11,7 +14,19 @@ class TimeoutError(Error):
 
 
 class InvalidResponseError(Error):
-    pass
+    path: Optional[str]
+    attribute: Optional[Any]
+
+    def __init__(self, message: str, path: Optional[str] = None, attribute: Optional[Any] = None):
+        super().__init__(message)
+        self.path = path
+        self.attribute = attribute
+
+    def __str__(self) -> str:
+        if self.path is not None and self.attribute is not None:
+            return f'{super().__str__()}: {self.path}={self.attribute}'
+
+        return super().__str__()
 
 
 class NotFoundError(Error):
