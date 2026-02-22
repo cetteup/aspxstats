a3a99b913b82afbc91a597d031c5ff8cdc07b2dd
cetteup <me@cetteup.com>
2026-02-22 10:15:15 +0100

    tag: bump version to 0.6.1

--- aspxstats/__init__.py
+++ aspxstats/__init__.py
@@ -7,7 +7,7 @@ aspxstats.
 Python library for retrieving stats of Battlefield 2 and Battlefield 2142 players.
 """
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 __author__ = 'cetteup'
 __credits__ = 'wilson212'
 __all__ = [
